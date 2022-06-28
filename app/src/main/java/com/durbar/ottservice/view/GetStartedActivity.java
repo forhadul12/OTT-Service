9752beb3b89cde0b4a00c576aefdef370b1c867c
fitareq <fitareq@gmail.com>
2022-06-28 18:55:25 +0600

    initial commit

---
+++ app/src/main/java/com/durbar/ottservice/view/GetStartedActivity.java
@@ -0,0 +1,16 @@
+package com.durbar.ottservice.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.durbar.ottservice.R;
+
+public class GetStartedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_started);
+    }
+}
