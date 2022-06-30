05d479bf80a936322fb6b3538d903bdc49576982
fitareq <fitareq@gmail.com>
2022-06-30 18:17:07 +0600

    Design: GetStartedActivity, MainActivity.

--- app/src/main/java/com/durbar/ottservice/view/GetStartedActivity.java
+++ app/src/main/java/com/durbar/ottservice/view/GetStartedActivity.java
@@ -1,16 +1,31 @@
 package com.durbar.ottservice.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.durbar.ottservice.R;
 
 public class GetStartedActivity extends AppCompatActivity {
 
+    private ConstraintLayout constraintLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+        constraintLayout = findViewById(R.id.constraintLayout);
+
+        constraintLayout.setOnClickListener(view -> {
+            gotoMainActivity();
+        });
+
+    }
+
+    private void gotoMainActivity() {
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        this.finish();
     }
 }
