aa9f94ede37770bd1668dce04e3e33f4e9bfe43f
fitareq <fitareq@gmail.com>
2022-07-06 18:54:52 +0600

    Design: OTPActivity

---
+++ app/src/main/java/com/durbar/ottservice/view/OTPActivity.java
@@ -0,0 +1,16 @@
+package com.durbar.ottservice.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.durbar.ottservice.R;
+
+public class OTPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otpactivity);
+    }
+}
