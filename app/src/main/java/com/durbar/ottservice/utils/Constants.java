c29f080f10d20e16320999e77aa959aa3d588351
fitareq <fitareq@gmail.com>
2022-07-20 16:29:30 +0600

    design: MoreActivity

--- app/src/main/java/com/durbar/ottservice/utils/Constants.java
+++ app/src/main/java/com/durbar/ottservice/utils/Constants.java
@@ -4,4 +4,7 @@ public class Constants {
     //Retrofit
     public static final String BASE_URL = "BASE_URL";
 
+    //toolbar title key
+    public static final String TOOLBAR_TITLE_KEY = "TOOLBAR_TITLE_KEY";
+
 }
