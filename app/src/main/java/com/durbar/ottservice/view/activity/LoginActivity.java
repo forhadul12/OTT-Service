2f9a15c3d18eb97c894511b39fb3920025f49474
fitareq <fitareq@gmail.com>
2022-07-20 17:00:41 +0600

    redesign: LoginActivity.

--- app/src/main/java/com/durbar/ottservice/view/activity/LoginActivity.java
+++ app/src/main/java/com/durbar/ottservice/view/activity/LoginActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.durbar.ottservice.R;
+import com.durbar.ottservice.databinding.ActivityLoginBinding;
 
  public class LoginActivity extends AppCompatActivity {
 
+     private ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
